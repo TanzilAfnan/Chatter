31e6593bc8895c847f5174df9e8cf4a7674cc6e9
tanzil_afn <tanzil.afn@gmail.com>
2019-03-23 16:21:46 +0600

    1 to 1 chat added

--- index.js
+++ index.js
@@ -17,15 +17,32 @@ app.use(express.static('public'));
 let io = socket(server);
 
 io.on('connection', (socket) => {
+
     console.log('Socket Connection Established !');
 
-    socket.on('chat', (data)=> {
-        console.log(data);
-        io.sockets.emit('chat', data);
+    socket.on('join_room', room=>{
+        console.log(room);
+        socket.join(room);
     });
 
-    socket.on('typing', (data)=>{
-        console.log(data + ' is typing!');
-        socket.broadcast.emit('typing', data + ' is typing');
-    })
+    socket.on('chat', data => {
+
+        const {room, message, handle} = data;
+
+        console.log(message);
+        socket.to(room).emit('chat', 
+        {
+            message : message,
+            handle : handle
+        });
+        // io.sockets.emit('chat', message);
+    });
+
+    socket.on('typing', ({room, handler}) =>{
+        console.log(handler + ' is typing!');
+        socket.to(room).emit('typing', handler+ ' is typing')
+        // socket.broadcast.emit('typing', data + ' is typing');
+        
+})
+
 });
--- public/chat.js
+++ public/chat.js
@@ -5,29 +5,52 @@ const socket = io.connect('http://localhost:9000/');
 
 // DOM query
 const message = document.getElementById('message');
-const handle = document.getElementById('handle');
-const button = document.getElementById('send');
+const handle = document.getElementById('User');
+const send = document.getElementById('send');
 const output = document.getElementById('output');
 const feedback = document.getElementById('feedback');
+const room = document.getElementById('room');
+const joinRoomButton = document.getElementById('joinRoom');
 
-//Emit Events
-button.addEventListener('click', ()=> {
+joinRoomButton.onclick = ()=> {
+    console.log(room.value);
+    socket.emit('join_room', room.value);
+}
+
+// Emit Events
+send.addEventListener('click', ()=> {
     console.log('buttonPressed');
-    socket.emit('chat', {
+
+    output.innerHTML += '<p> <strong>' + handle.value + ':</strong>  '
+                        + message.value + '</p>'
+
+    socket.emit('chat', 
+    {
+        room : room.value,
         message : message.value,
         handle : handle.value
-    });
-});
+    }
+    );
+    message.value = "";
 
-message.addEventListener('keypress', ()=> {
-    socket.emit('typing', handle.value);
-})
+});
 
 // Listen for events
 socket.on('chat', (data)=>{
-    output.innerHTML += '<p> <strong>' + data.handle + ':</strong>'
+
+    output.innerHTML += '<p> <strong>' + data.handle + ':</strong>  '
                         + data.message + '</p>'
     console.log(data);
+    feedback.innerHTML = '';
+})
+
+message.addEventListener('keypress', ()=> {
+
+    console.log("handle.value");
+    socket.emit('typing', {
+        room : room.value,
+        handler : handle.value
+    });
 })
 
 socket.on('typing', (data)=> {
